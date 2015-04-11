29f8082d77e4f22c1999a341d6bb0e8080ea8d87
Iiro Surakka <iiro.surakka@gmail.com>
2015-04-11 16:32:46 +0300

    Add initial files

---
+++ CarSimulator/Program.cs
@@ -0,0 +1,28 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rw = new RenderWindow(new VideoMode(1024, 768), "Car Simulator", Styles.Close, new ContextSettings { AntialiasingLevel = 8 });
+            rw.Closed += (s, a) => rw.Close();
+
+            while (rw.IsOpen)
+            {
+                rw.Clear();
+
+                rw.DispatchEvents();
+
+                rw.Display();
+            }
+        }
+    }
+}
